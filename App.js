731e679088015bcbcaaa0a6d32cac5cd0e47ca62
Adnan Saleem <adnan13893@gmail.com>
2024-03-06 01:59:28 +0500

    learning react-native from scratch

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 
+const COLORS = [
+  'red',
+  'green',
+  'blue',
+  'yellow',
+  'white',
+  'pink',
+  'turquoise',
+  'orange',
+  'purple',
+  'black'
+]
 export default function App() {
+  const [bgColor, setBgColor] = useState(COLORS[0])
+  const toggleBackground = () => {
+    const number = Math.floor(Math.random() * 10)
+    setBgColor(COLORS[number - 1])
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        ...styles.mainContainer,
+        backgroundColor: bgColor
+      }}
+    >
+      <TouchableOpacity onPress={toggleBackground} style={styles.button}>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mainContainer: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  button: {
+    backgroundColor: 'black',
+    padding: 12,
+    borderRadius: 12,
+    borderColor: 'gray',
+    borderWidth: 2
   },
-});
+  buttonText: {
+    color: 'gray',
+    fontSize: 20
+  }
+})
